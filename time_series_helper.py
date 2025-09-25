9f26b4344b92724ffd6af8b843cfd6639ac3fc0a
Sarah Nabelsi <snabelsi@Sarahs-MacBook-Pro-2.local>
2025-09-25 08:25:05 -0400

    adding files

--- time_series_helper.py
+++ time_series_helper.py
@@ -4,14 +4,23 @@ import seaborn as sns
 import numpy as np
 from statsmodels.nonparametric.smoothers_lowess import lowess
 from statsmodels.graphics.tsaplots import plot_acf
+from typing import Tuple
 
 
-def lineplot(df, column):
+def lineplot(data, column):
+    """
+    Creates a line plot to visualize a time series
 
-    # Plotting the time series
+    Args:
+        data (pd.DataFrame): A DataFrame with a DatetimeIndex.
+        column (str): The name of the column to plot.
+    """
+
+    # Plot a time series 
     plt.figure(figsize=(10, 6))
-    plt.plot(df.index, df[column], color='black', alpha=0.5, linewidth=1)
+    plt.plot(data.index, data[column], color='black', alpha=0.5, linewidth=1)
 
+    # Customize the plot's appearance
     title = 'Line Plot of' + " "+ column
     plt.title(title, fontsize=16)
     plt.xlabel('Month', fontsize=12)
@@ -23,21 +32,34 @@ def lineplot(df, column):
 
 
 def seasonalplot(data, column, max_val=10):
-    
-    # Creating a seasonal plot
-    # First, we need to add a 'year' and 'month' column
+    """
+    Creates a seasonal plot to visualize data distribution across months, 
+    with each year represented by a separate line.
+
+    Args:
+        data (pd.DataFrame): A DataFrame with a DatetimeIndex.
+        column (str): The name of the column to plot.
+        max_val (numeric): represents last X years
+    """
+
+    # Create a copy of the dataframe
     df = data.copy()
 
+    # Create month and year variables
     df['year'] = df.index.year
     df['month'] = df.index.month
 
+    # Filter the data to the most recent 10 years
     df = df[df['year']>(df['year'].max()-10)]
 
+    # Aggregate data to month and year
     df = df.groupby(['month', 'year']).mean().reset_index()
 
+    # Plot the time series, with hue='year' 
     plt.figure(figsize=(10, 6))
     sns.lineplot(data=df, x='month', y=column, hue='year', legend='full', palette='tab10')
 
+    # Customize the plot's appearance
     title = 'Seasonal Plot of' + " "+ column
     plt.title(title, fontsize=16)
     plt.xlabel('Month')
@@ -60,6 +82,7 @@ def seasonalboxplot(data, column):
         data (pd.DataFrame): A DataFrame with a DatetimeIndex.
         column (str): The name of the column to plot.
     """
+
     # Make a copy of the original data to avoid modifying it
     df = data.copy()
 
@@ -68,15 +91,11 @@ def seasonalboxplot(data, column):
     df['month'] = df.index.month
     
     # Filter the data to the most recent 10 years
-    # This matches the behavior of the original function
-    current_year = df['year'].max()
+    # current_year = df['year'].max()
     # df = df[df['year'] > (current_year - 10)]
 
-    # Set up the plot size
-    plt.figure(figsize=(10, 6))
-    
     # Create the box plot using Seaborn
-    # The 'hue' parameter is used to create a separate box for each year within each month
+    plt.figure(figsize=(10, 6))
     sns.boxplot(data=df, x='month', y=column, palette='tab10')
     
     # Customize the plot's appearance
@@ -95,17 +114,28 @@ def seasonalboxplot(data, column):
 
 
 
-def ma_plot(df, column, ma=7):
+def ma_plot(data, column, ma=7):
+    """
+    Creates a line plot with moving average overlay.
 
-    data = df.copy()
+    Args:
+        data (pd.DataFrame): A DataFrame with a DatetimeIndex.
+        column (str): The name of the column to plot.
+        ma (number): Represents the number of data points for the moving average
+    """
+
+    # Make a copy of the original data to avoid modifying it
+    df = data.copy()
+
     # Calculate the moving averages
-    data['MA_'+str(ma)] = data[column].rolling(window=ma).mean()
+    df['MA_'+str(ma)] = df[column].rolling(window=ma).mean()
 
     # Plot the data
     plt.figure(figsize=(10, 6))
-    plt.plot(data[column], label=column, color='black', alpha=0.5, linewidth=1)
-    plt.plot(data['MA_'+str(ma)], label=str(ma)+'-Month MA', linestyle=':', color='g')
+    plt.plot(df[column], label=column, color='black', alpha=0.5, linewidth=1)
+    plt.plot(df['MA_'+str(ma)], label=str(ma)+'-Month MA', linestyle=':', color='g')
 
+    # Customize the plot's appearance
     title = column + ' with ' + str(ma) + 'Month Moving Average'
     plt.title(title, fontsize=16)
     plt.xlabel('Date', fontsize=12)
@@ -118,17 +148,25 @@ def ma_plot(df, column, ma=7):
     plt.show()
 
 
-def lowess_plot(df, column, frac=0.2):
+def lowess_plot(data, column, frac=0.2):
+    """
+    Creates a line plot with moving average overlay.
 
-    loess_smoothed = lowess(df[column], df.index, frac=0.2, it=0)
+    Args:
+        data (pd.DataFrame): A DataFrame with a DatetimeIndex.
+        column (str): The name of the column to plot.
+        frac (float): We use a fraction of the data for the span to control smoothness
+    """
+
+    # Calculate the LOESS smooth curve
+    loess_smoothed = lowess(data[column], data.index, frac=frac, it=0)
     loess_df = pd.DataFrame(loess_smoothed, columns=['date', 'loess_'+column])
     loess_df['date'] = pd.to_datetime(loess_df['date'])
     loess_df = loess_df.set_index('date')
 
-    plt.figure(figsize=(10, 6))
-
     # Plot the original data
-    plt.plot(df.index, df[column], label='Observed Data', color='black', alpha=0.5, linewidth=1)
+    plt.figure(figsize=(10, 6))
+    plt.plot(data.index, data[column], label='Observed Data', color='black', alpha=0.5, linewidth=1)
 
     # Plot the LOESS smooth curve
     plt.plot(loess_df.index, loess_df['loess_'+column], label='LOESS Smooth', color='blue', linewidth=2)
@@ -147,27 +185,40 @@ def lowess_plot(df, column, frac=0.2):
 
 
 def decomp_plots(d):
-    # plot the decomposition `sharex=True` argument ensures the x-axes are aligned.
+    """
+    Takes a time series and decomposes it into: trend, seaonality, and residual
+
+    Args:
+        d (DecomposeResult): Time series decomposition
+    """
+
+    # Plot the decomposition `sharex=True` argument ensures the x-axes are aligned.
     fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(10, 10), sharex=True)
 
-    # plot each component on its own subplot.
-    # the decomposition object has attributes for 'observed', 'trend', 'seasonal', and 'resid'.
+    # Plot each component on its own subplot.
+    # The decomposition object has attributes for 'observed', 'trend', 'seasonal', and 'resid'.
     d.observed.plot(ax=ax1, title='Observed', color='black')
     d.trend.plot(ax=ax2, title='Trend', color='blue')
     d.seasonal.plot(ax=ax3, title='Seasonal', color='green')
     d.resid.plot(ax=ax4, title='Residual', color='red')
 
-    # add a main title to the figure.
+    # Add a main title to the figure.
     fig.suptitle('Classical Time Series Decomposition of co2', y=1.02, fontsize=16)
 
-    # improve layout to prevent titles and labels from overlapping.
+    # Improve layout to prevent titles and labels from overlapping.
     plt.tight_layout(rect=[0, 0, 1, 0.98])
-
-    # display the plot
     plt.show()
 
 
 def resid_plot(residuals):
+    """
+    Takes the residuals from the time series decomposition 
+    and returns time plot, histogram and ACF plot
+
+    Args:
+        residuals (Series): Time series decomposition
+    """
+
     # Create a figure with three subplots
     fig, axes = plt.subplots(2, 1, figsize=(10, 8))
 
@@ -193,39 +244,54 @@ def resid_plot(residuals):
     plt.show()
 
 
-import pandas as pd
-
-def split_train_test(df, n=12):
+def train_test_split(df: pd.DataFrame, test_size: float = 0.25) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """
-    Splits a DataFrame into a training and testing set.
+    Splits a time series dataframe into training and testing sets.
     
-    The function tries to use a training set that is 10 times the size of the test set,
-    but falls back to a smaller train set if the data is too short.
-    
+    The split is performed by reserving a specified percentage of the
+    *latest* data points for the test set, which is crucial for
+    time series analysis to prevent look-ahead bias.
+
     Args:
-        df (pd.DataFrame): The DataFrame to split.
-        n (int): The number of rows for the test set.
-        
+        df (pd.DataFrame): The input time series DataFrame.
+        test_size (float): The proportion of the dataset to include in the 
+                           test split (e.g., 0.25 for 25%). Defaults to 0.25.
+
     Returns:
-        tuple: A tuple containing the training and testing DataFrames.
+        Tuple[pd.DataFrame, pd.DataFrame]: A tuple containing the 
+                                           (train_set, test_set) DataFrames.
+    
+    Raises:
+        ValueError: If test_size is not between 0 and 1 (exclusive).
     """
-    # Calculate the number of rows for the training set.
-    # The training set is 10 times the test set, but it can't be more than the total rows minus the test set.
-    train_size = max(df.shape[0] - n, n)
+    if not (0 < test_size < 1):
+        raise ValueError("test_size must be a float between 0 and 1 (exclusive).")
 
-    # Check if a training set of 10 * n is possible
-    if df.shape[0] > (n * 30):
-        train_data = df.iloc[len(df) - (30*5):-n]
-        test_data = df.iloc[-n:]
-    else:
-        # If not, use a simple split
-        train_data = df.iloc[:-n]
-        test_data = df.iloc[-n:]
+    # Calculate the size of the test set
+    test_rows = int(len(df) * test_size)
+    
+    # The split point index is the start of the test set
+    split_index = len(df) - test_rows
+    
+    # Split the DataFrame
+    train_set = df.iloc[:split_index]
+    test_set = df.iloc[split_index:]
+    
+    return train_set, test_set
 
-    return train_data, test_data
 
+def simple_forecasts(train_data, test_data, column):
+    """
+    Takes the training and testing set of a time series 
+    and generates simple forecasts (mean, naive and seasonal naive)
 
-def simple_forecasts(train_data, test_data, column, n=12):
+    Args:
+        train_data (pd.DataFrame): Training data
+        test_data (pd.DataFrame): Test data
+        column (str): The name of the column to forecast
+    """
+
+    n = len(test_data)
 
     # 3a. Mean Forecast
     mean_forecast_value = train_data[column].mean()
@@ -244,9 +310,18 @@ def simple_forecasts(train_data, test_data, column, n=12):
     return mean_forecast, naive_forecast, seasonal_naive_forecast
 
 
-def plot_forecasts(train_data, test_data, column, n=12):
+def plot_forecasts(train_data, test_data, column):
+    """
+    Plots the training and testing set of a time series 
+    along with simple forecasts (mean, naive and seasonal naive)
 
-    mean_forecast, naive_forecast, seasonal_naive_forecast = simple_forecasts(train_data, test_data, column, n)
+    Args:
+        train_data (pd.DataFrame): Training data
+        test_data (pd.DataFrame): Test data
+        column (str): The name of the column to forecast
+    """
+
+    mean_forecast, naive_forecast, seasonal_naive_forecast = simple_forecasts(train_data, test_data, column)
 
     plt.figure(figsize=(10, 6))
 
@@ -273,9 +348,17 @@ def plot_forecasts(train_data, test_data, column, n=12):
     plt.show()
 
 
-def calc_errors(train_data, test_data, column, n=12):
+def calc_errors(train_data, test_data, column):
+    """
+    Calculates errors related to simple forecasts
 
-    mean_forecast, naive_forecast, seasonal_naive_forecast = simple_forecasts(train_data, test_data, column, n)
+    Args:
+        train_data (pd.DataFrame): Training data
+        test_data (pd.DataFrame): Test data
+        column (str): The name of the column to forecast
+    """
+
+    mean_forecast, naive_forecast, seasonal_naive_forecast = simple_forecasts(train_data, test_data, column)
 
     # Create a dictionary to hold metrics
     metrics = {
@@ -301,47 +384,3 @@ def calc_errors(train_data, test_data, column, n=12):
     results_df = results_df.set_index('Model')
 
     return results_df
-
-# def create_lag_plots(dataframe, column, max_lags=6):
-#     """
-#     Creates a grid of lag plots for a specified time series column.
-
-#     Parameters:
-#     - dataframe (pd.DataFrame): The DataFrame containing the time series data.
-#     - column (str): The name of the column to plot.
-#     - max_lags (int): The maximum number of lags to plot.
-#     """
-#     # Create a figure with subplots
-#     fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(15, 10))
-#     fig.suptitle(f'Lag Plots for {column}', fontsize=18, y=1.02)
-    
-#     # Flatten the axes array for easy iteration
-#     axes = axes.flatten()
-
-#     for i in range(1, max_lags + 1):
-#         ax = axes[i-1]
-        
-#         # Create a new column with the lagged data
-#         lagged_data = dataframe[column].shift(i)
-        
-#         # Plot the data
-#         ax.scatter(lagged_data, dataframe[column], alpha=0.6)
-        
-#         # Add labels and title
-#         ax.set_title(f'Lag {i}', fontsize=14)
-#         ax.set_xlabel(f'{column} (t-{i})')
-#         ax.set_ylabel(f'{column} (t)')
-        
-#         # Add a diagonal line for reference (x=y)
-#         min_val = min(dataframe[column].min(), lagged_data.min())
-#         max_val = max(dataframe[column].max(), lagged_data.max())
-#         ax.plot([min_val, max_val], [min_val, max_val], color='red', linestyle='--', linewidth=1)
-#         ax.grid(True, linestyle='--', alpha=0.5)
-
-#     # Remove any unused subplots
-#     for j in range(len(axes)):
-#         if j >= max_lags:
-#             fig.delaxes(axes[j])
-            
-#     plt.tight_layout()
-#     plt.show()
